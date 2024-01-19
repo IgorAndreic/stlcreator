d9073a81a99ad96a958d1a9d23c2ee15f71c14b2
Administrator <Administrator@PHURGVSVTN>
2024-01-20 01:24:34 +0200

    Добавьте файлы проекта.

---
+++ js/locales.js
@@ -0,0 +1,22 @@
+﻿function loadLocalization() {
+    const langCode = localStorage.getItem('selectedLanguage') || 'en'; // Значение по умолчанию - английский
+    fetch(`../locales/${langCode}/translation.json`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll("[data-localize]").forEach(el => {
+                el.textContent = data[el.dataset.localize];
+            });
+
+            // Получаем название файла из URL (например, 'contact' из 'contact.html')
+            let pageName = window.location.pathname.split('/').pop().split('.').shift();
+            let pageTitleKey = `${pageName}Title`;
+
+            // Обновляем заголовок страницы
+            if (data[pageTitleKey]) {
+                document.title = data[pageTitleKey];
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка при загрузке файла локализации:', error);
+        });
+}
---
+++ js/script.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('language-selector').addEventListener('change', function () {
+    loadLocalization(this.value);
+});
+
+function loadLocalization(langCode) {
+    fetch(`locales/${langCode}/translation.json`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll("[data-localize]").forEach(el => {
+                el.textContent = data[el.dataset.localize];
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при загрузке файла локализации:', error);
+        });
+}
+
+// Загрузка локализации по умолчанию при первоначальной загрузке страницы
+//window.onload = () => loadLocalization('en'); // Или другой язык по умолчанию
